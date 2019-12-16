3405b5fa419f8516a74d054774324b6e42bca6e3
David Mears <kieukmail@gmail.com>
2019-12-16 14:30:38 +0000

    Feature test (red) for adding 2 names and getting the recipient.

--- spec/features/add_emails_spec.rb
+++ spec/features/add_emails_spec.rb
@@ -1,5 +1,14 @@
 describe "Adding email addresses", type: :feature do
-    xscenario "A user can add a list of email addresses and see them displayed" do
-        
+    scenario "A user can add a list of two names/emails and see their recipient" do
+        visit '/participants/new'
+        fill_in("name", with: "David")
+        fill_in("email", with: "david@david.david")
+        check "Is this you?"
+        click_button("Submit")
+        fill_in("name", with: "Josh")
+        fill_in("email", with: "josh@josh.josh")
+        click_button("Submit")
+        click_button("Assign pairs")
+        expect(page).to have_content "Your recipient is Josh!"
     end
 end
